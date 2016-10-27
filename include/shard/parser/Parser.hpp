5a9791071bf5daf92d6ca4a83471b1038d5a4212
Vaclav Pelisek <peldax@gmail.com>
2016-10-28 01:04:31 +0200

    added parser classes

---
+++ include/shard/parser/Parser.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************* */
+/* This file is part of Shard.                                               */
+/*                                                                           */
+/* Shard is free software: you can redistribute it and/or modify             */
+/* it under the terms of the GNU Affero General Public License as            */
+/* published by the Free Software Foundation.                                */
+/*                                                                           */
+/* This program is distributed in the hope that it will be useful,           */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              */
+/* GNU Affero General Public License for more details.                       */
+/*                                                                           */
+/* You should have received a copy of the GNU Affero General Public License  */
+/* along with this program. If not, see <http://www.gnu.org/licenses/>.      */
+/* ************************************************************************* */
+
+#pragma once
+
+/* ************************************************************************* */
+
+// Shard
+#include "shard/tokenizer/Tokenizer.hpp"
+
+/* ************************************************************************* */
+
+namespace shard {
+inline namespace v1 {
+namespace parser {
+
+using namespace tokenizer;
+using namespace ast;
+
+/* ************************************************************************* */
+
+class Parser
+{
+
+protected:
+
+	Tokenizer m_tokenizer;
+
+};
+
+/* ************************************************************************* */
+
+}
+}
+}
+
+/* ************************************************************************* */
