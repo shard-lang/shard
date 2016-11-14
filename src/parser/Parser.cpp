16d70811ed04ba9c584706fd2a6519a3efe17a26
Vaclav Pelisek <peldax@gmail.com>
2016-11-15 00:56:57 +0100

    removed unnecessary includes

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -18,14 +18,6 @@
 
 /* ************************************************************************* */
 
-#include "shard/UniquePtr.hpp"
-#include "shard/PtrDynamicArray.hpp"
-#include "shard/tokenizer/Tokenizer.hpp"
-#include "shard/tokenizer/TokenType.hpp"
-#include "shard/ast/Module.hpp"
-#include "shard/ast/Stmt.hpp"
-#include "shard/ast/Expr.hpp"
-#include "shard/ast/Decl.hpp"
 #include "shard/parser/ParserException.hpp"
 
 /* ************************************************************************* */
